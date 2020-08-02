c77a2abeb42d5a39bbb1c7e61355ff8c62f480ca
lenivec <wormsom@gmail.com>
2020-08-02 20:13:43 +0300

    added pagination

--- django_movie_library_rest/settings.py
+++ django_movie_library_rest/settings.py
@@ -237,7 +237,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 1
 }
 
 # EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
